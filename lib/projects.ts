5ead04ea9b102a324b79d77bb6fde45f12b5fbf8
ShivaTejMatam <matamsivateja@gmail.com>
2025-04-26 15:56:10 +0530

    feat:Added New Project

--- lib/projects.ts
+++ lib/projects.ts
@@ -7,18 +7,18 @@ export interface Project {
   
   export const projects: Project[] = [
     {
+      title: "Social Media App",
+      description:
+      "Developed a Social Media Platform using Nextjs and TypeScript, enabling user authentication with clerk and and realtime data fetching with Prisma ORM and PostgreSQL, allowing users to create posts, like, comment, and follow others and can upload images.",
+      link: "https://social-mediahub.vercel.app/",
+      techStacks: ["Next Js","Tailwind Css","TypeScript","Prisma ORM","PostgreSQL","Clerk","Next Auth","Node Js","Uploadthing"]
+    },
+    {
       title: "PostHive",
       description:
          "Developed a full-stack blog platform with React Js and TypeScript, enabling seamless post creation, likes, follows, and user authentication with JWT, powered by PostgreSQL and Prisma ORM for efficient data management.",    
       link: "https://github.com/ShivaTejMatam/PostHive",
       techStacks: [ "React Js","Tailwind Css","TypeScript","Prisma ORM","PostgreSQL","Node Js","Express Js","JWT"]
-    },
-    {
-      title: "Real-time Chat application",
-      description:
-      "Real-time chat application with Socket.io featuring room-based messaging, user presence indicators, and instant message delivery. Supports multiple chat rooms and persistent chat history.",
-      link: "https://github.com/ShivaTejMatam/Chat-Application",
-      techStacks: ["React Js","Tailwind Css","Node Js","Socket.io","Express Js","JavaScript","HTML","CSS"]
     },
     {
       title:"Event Hosting ",
